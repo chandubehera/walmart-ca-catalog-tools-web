ecd0ffc2a2234f1d77640251b19e9f0a0426acc2
Chandrakanta Behera <chandrakantab@maveric-systems.com>
2025-03-12 11:25:03 +0530

    creating new repo for walmart-ca-catalog-tools-web application

---
+++ src/main/java/ca/catalog/tools/web/WalmartCaCatalogToolsWebApplication.java
@@ -0,0 +1,13 @@
+package ca.catalog.tools.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalmartCaCatalogToolsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalmartCaCatalogToolsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/catalog/tools/web/WalmartCaCatalogToolsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.catalog.tools.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalmartCaCatalogToolsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
